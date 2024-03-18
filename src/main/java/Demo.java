85e6cef23b22443f1c4fd7373ad805cb266d5d14
James <709914+Jooms@users.noreply.github.com>
2024-03-18 12:22:11 -0700

    cluster file

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,5 +1,10 @@
+import com.apple.foundationdb.record.provider.foundationdb.FDBDatabase;
+import com.apple.foundationdb.record.provider.foundationdb.FDBDatabaseFactory;
+
 public class Demo {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
+
+        FDBDatabase db = FDBDatabaseFactory.instance().getDatabase("fdb_config.cluster");
     }
 }
